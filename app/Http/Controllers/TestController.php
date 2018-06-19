3703f6c4c0396ff1ed634a77f38e6321dd38ef47
Siarhei Sharykhin <chapal@inbox.ru>
2018-06-19 17:06:03 +0300

    ha

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -12,6 +12,30 @@ class TestController extends Controller
     {
         $i= 10;
         $b = 20;
+
+        $c = 10;
+        $d = $c / 2;
+        $e = $d / 2;
+        $t = $e / 2;
+
+        $c = 10;
+        $d = $c / 2;
+        $e = $d / 2;
+        $t = $e / 2;
+
         echo "12";
+    }
+
+    public function bad()
+    {
+        $c = 10;
+        $d = $c / 2;
+        $e = $d / 2;
+        $t = $e / 2;
+
+        $c = 10;
+        $d = $c / 2;
+        $e = $d / 2;
+        $t = $e / 2;
     }
 }
